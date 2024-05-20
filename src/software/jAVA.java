74215b3bb69d8a466173a607a2ab15ad2c81599f
ayyas <ayyashfaiz956@gmail.com>
2024-05-20 15:52:01 +0530

    my first commit

---
+++ src/software/jAVA.java
@@ -0,0 +1,17 @@
+package software;
+
+public class jAVA {
+   ///attributes
+   //syntax type + Name + value;
+
+   String Name;
+
+   String Popgramming_language;
+
+   String fav_codingpro;
+
+   //constructor
+   //syntax of constructor
+
+
+}
